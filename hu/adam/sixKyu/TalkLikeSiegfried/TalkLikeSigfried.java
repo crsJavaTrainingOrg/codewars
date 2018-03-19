ba69e9d3ad1a9f97f77a08e649cdffbed57426e1
siposadam97 <siposadam.97@gmail.com>
2018-03-19 21:01:09 +0100

    TalkLikeSiegfried

---
+++ hu/adam/sixKyu/TalkLikeSiegfried/TalkLikeSigfried.java
@@ -0,0 +1,42 @@
+package sixKyuExercises.TalkLikeSiegfried;
+
+public class TalkLikeSigfried {
+    public static String siegfried(final int week, final String str) {
+
+        String[] words = str.split(" ");
+
+        String result = "";
+
+            if (week >= 1) {
+                result = str.replaceAll("ci","si");
+                result = result.replaceAll("ce","se");
+                result = result.replaceAll("c(?!h)","k");
+
+            }
+            if(week >= 2){
+                result = result.replaceAll("ph","f");
+
+            }
+            if(week >=3){
+                result = result.replaceAll("e+(?<=\\w{4})\b", "");
+                for(int k = 0; k < words.length; k++){
+                    words[k] = words[k].replaceAll("(.)//1","$1");
+                }
+            }
+            if(week >= 4){
+                result = result.replaceAll("th","z");
+                result = result.replaceAll("wr","v");
+                result = result.replaceAll("w","v");
+            }
+            if(week >= 5){
+                result = result.replaceAll("ou","u");
+                result = result.replaceAll("an","un");
+                result = result.replaceAll("ing*$","ink");
+                result = result.replaceAll("^sm*","schm");
+
+
+        }
+        return result;
+    }
+}
+
---
+++ hu/adam/sixKyu/TalkLikeSiegfried/TalkLikeSigfriedTest.java
@@ -0,0 +1,10 @@
+package sixKyuExercises.TalkLikeSiegfried;
+
+import sixKyuExercises.TalkLikeSiegfried.TalkLikeSigfried;
+
+public class TalkLikeSigfriedTest {
+    public static void main(String[] args){
+        System.out.println(TalkLikeSigfried.siegfried(5, "\"This is KAOS!! We don't play with Code-Wars here!! \""));
+
+    }
+}
